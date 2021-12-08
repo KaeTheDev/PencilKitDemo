b75d9c262c4d882c859103b449f8e6b47c13772a
ShakiraReidThomas <kaethedev@gmail.com>
2021-12-08 14:32:32 -0500

    Initial Commit

---
+++ PencilKitDemo/PencilKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitDemoApp.swift
+//  PencilKitDemo
+//
+//  Created by Shakira Reid-Thomas on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
